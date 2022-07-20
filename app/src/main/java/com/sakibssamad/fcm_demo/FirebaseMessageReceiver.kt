bc655f71d29dce0496e5050d2a3a9098a41aa056
sakibssamad <sssakib.info@gmail.com>
2022-07-20 17:02:56 +0600

    Initial commit

---
+++ app/src/main/java/com/sakibssamad/fcm_demo/FirebaseMessageReceiver.kt
@@ -0,0 +1,100 @@
+package com.sakibssamad.fcm_demo
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+
+
+
+const val channelId = "notification_channel"
+const val channelName = "com.sakibssamad.fcm_demo"
+
+class FirebaseMessageReceiver : FirebaseMessagingService() {
+
+    // Generate The Notification
+    // Attach the notification created with the custom layout
+    // show the notification
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+        if (remoteMessage.notification != null) {
+            generateNotification(
+                remoteMessage.notification!!.title!!,
+                remoteMessage.notification!!.body!!
+            )
+        }
+    }
+
+    private fun getRemoteView(
+        title: String,
+        message: String
+    ): RemoteViews? {
+        val remoteViews = RemoteViews("com.sakibssamad.fcm_demo", R.layout.notification)
+        remoteViews.setTextViewText(R.id.title, title)
+        remoteViews.setTextViewText(R.id.message, message)
+        remoteViews.setImageViewResource(
+            R.id.app_logo,
+            R.drawable.github
+        )
+        return remoteViews
+    }
+
+
+    fun generateNotification(title: String, message: String) {
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        //pending intent use for the comming future
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        //channel Id, channel name
+
+        var builder: NotificationCompat.Builder =
+            NotificationCompat.Builder(applicationContext, channelId)
+                .setSmallIcon(R.drawable.github)
+                .setAutoCancel(true)
+                .setVibrate(
+                    longArrayOf(
+                        1000, 1000, 1000,
+                        1000, 1000
+                    )
+                )
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            builder = builder.setContent(getRemoteView(title, message))
+        }
+        else {
+            builder = builder.setContentTitle(title).setContentText(message).setSmallIcon(R.drawable.github);
+        }
+
+
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId, channelName, NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        notificationManager.notify(0, builder.build())
+
+    }
+
+
+
+
+
+}
